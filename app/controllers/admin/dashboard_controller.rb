92f35b3610ead41df779be2d838bd1297292b5c0
Tammy Tran <tammy.n.tran@gmail.com>
2021-02-04 04:08:17 +0000

    updated dashboard to include product count info and number of categories

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,4 +1,8 @@
 class Admin::DashboardController < ApplicationController
   def show
+    @product_count = Product.sum(:quantity)
+    @category_count = Category.count
   end
+  # STRETCH TO DO: add link to admin/products list and catergoy index pages
+  # make it pretty with 3 bootstrap elements
 end
