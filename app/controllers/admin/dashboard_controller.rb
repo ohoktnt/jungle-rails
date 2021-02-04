cccaf89f726391b730748f38a2b5e69d02de7ba9
Tammy Tran <tammy.n.tran@gmail.com>
2021-02-04 16:18:32 +0000

    added links to products and categories for dashboard

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -2,6 +2,7 @@ class Admin::DashboardController < ApplicationController
   def show
     @product_count = Product.sum(:quantity)
     @category_count = Category.count
+    @categories = Category.all
   end
   # STRETCH TO DO: add link to admin/products list and catergoy index pages
   # make it pretty with 3 bootstrap elements
